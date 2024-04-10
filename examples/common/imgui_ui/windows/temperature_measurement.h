9b3051f3934a60e50dacad659999f46bb9d5b934
null <null@email.com>
2024-04-10 00:50:43 -0400

    minor bug fix

--- examples/common/imgui_ui/windows/temperature_measurement.h
+++ examples/common/imgui_ui/windows/temperature_measurement.h
@@ -34,7 +34,7 @@ namespace Windows {
 class TemperatureMeasurement : public Window
 {
 public:
-    TemperatureMeasurement(chip::EndpointId endpointId) : mEndpointId(endpointId), mTitle("Temperature Measurement") {}
+    TemperatureMeasurement(chip::EndpointId endpointId, const char * title) : mEndpointId(endpointId), mTitle(title) {}
     void UpdateState() override;
     void Render() override;
 
