f6957843250a0dd4f3d7e101553bf4eafe08deab
Your Name <you@example.com>
2024-04-14 16:37:11 -0400

    update locking logic

--- examples/common/pigweed/rpc_services/Locking.h
+++ examples/common/pigweed/rpc_services/Locking.h
@@ -1,23 +1,4 @@
-/*
- *
- *    Copyright (c) 2021 Project CHIP Authors
- *    All rights reserved.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
 #pragma once
-
 #include "app/util/attribute-storage.h"
 #include "locking_service/locking_service.rpc.pb.h"
 #include "pigweed/rpc_services/internal/StatusUtils.h"
@@ -32,25 +13,32 @@ class Locking final : public pw_rpc::nanopb::Locking::Service<Locking>
 public:
     virtual ~Locking() = default;
 
-    virtual pw::Status Set(const chip_rpc_LockingState & request, pw_protobuf_Empty & response)
+    virtual pw::Status Set(const chip_rpc_LockingSetRequest & request, chip_rpc_LockingSetResponse & response)
     {
-        bool locked = request.locked;
-        DeviceLayer::StackLock lock;
-        RETURN_STATUS_IF_NOT_OK(app::Clusters::OnOff::Attributes::OnOff::Set(kEndpoint, locked));
+        EndpointId endpointId = request.endpoint_id;
+        uint32_t newState         = request.state_value;
+
+        EventNumber eventNumber;
+        {
+            DeviceLayer::StackLock lock;
+            RETURN_STATUS_IF_NOT_OK(app::Clusters::DoorLock::Attributes::LockState::Set(endpointId, static_cast<chip::app::Clusters::DoorLock::DlLockState>(newState)));
+        }
+
+        response.event_number = static_cast<uint64_t>(eventNumber);
         return pw::OkStatus();
     }
 
-    virtual pw::Status Get(const pw_protobuf_Empty & request, chip_rpc_LockingState & response)
+    virtual pw::Status Get(const chip_rpc_LockingGetRequest & request, chip_rpc_LockingState & response)
     {
-        bool locked;
-        DeviceLayer::StackLock lock;
-        RETURN_STATUS_IF_NOT_OK(app::Clusters::OnOff::Attributes::OnOff::Get(kEndpoint, &locked));
-        response.locked = locked;
+        EndpointId endpointId = request.endpoint_id;
+        chip::app::DataModel::Nullable<chip::app::Clusters::DoorLock::DlLockState> state_value;
+        {
+            DeviceLayer::StackLock lock;
+            RETURN_STATUS_IF_NOT_OK(app::Clusters::DoorLock::Attributes::LockState::Get(endpointId, state_value));
+        }
+        response.state_value = static_cast<uint32_t>(state_value.Value());
         return pw::OkStatus();
     }
-
-private:
-    static constexpr EndpointId kEndpoint = 1;
 };
 
 } // namespace rpc
